8d4937a2a6a64c308486624a35a844d80facb038
github username <github email address>
2019-06-26 19:26:30 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,29 @@
+class Book 
+    def initialize(title)
+        @title = title
+    end 
+
+    def title
+        @title
+    end
+    
+    def author=(author) # This method is what allows me to to set the instance variable @author equal to something from the outside. I am able to change this instance variable because this method allows me to do so. This is my setter method. 
+        @author = author 
+    end 
+
+    def author # This method is what allows me to tell what the instance variable actually is. I can call .author on the instance of the class book and it will tell me who the author is. 
+        @author
+    end 
+
+    attr_accessor :page_count # this accomplishes what the setter andf getter method would do. Allows me to set the page count and retrieve it. 
+
+    attr_accessor :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+
+
+end 
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,34 @@
 # Make your shoe class here!
+require 'pry'
+class Shoe 
+    attr_accessor :brand #.brand would return the brand that the instance of shoe was initialized with
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material 
+    attr_accessor :condition
+
+
+    def initialize(brand)
+        @brand = brand 
+    end 
+
+    def cobble 
+        self.condition = "new"
+        puts "Your shoe is as good as new!"
+    end 
+
+
+
+
+
+
+
+end 
+
+
+
+#shoe = Shoe.new("Jordan")
+#shoe.condition = "old"
+#puts shoe.condition
+#puts shoe.cobble
+#puts shoe.condition
