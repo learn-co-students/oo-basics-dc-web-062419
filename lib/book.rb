c524311acedeeaa278754af903c2eda31109bd38
Trevahr Robertson <Trevahr@gmail.com>
2019-06-26 14:37:12 -0400

    work in progress

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+class Book
 
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title 
+    end
+
+
+  attr_writer :page_count, :author, :genre
+  attr_reader :page_count, :author, :genre 
+
+  
+
+
+end
 
